e4cbcef8c62d1d057ffaaf58b47fc5a1ffdee128
Adriel Alberto <96995025+adrielldev@users.noreply.github.com>
2022-02-17 10:43:19 -0300

    Update create.js

--- create.js
+++ create.js
@@ -1,6 +1,30 @@
 var fs = require('fs');
 const prompt = require('prompt-sync')();
 console.log('Bem vindo ao create web project');
+
+createWebProject();
+
+function createWebProject(){
+   
+   console.log("Default names:\nindex.html\nstyle.css\nscript.js\nVocê pode renomear dps");
+   nomeDoProjeto = prompt('Nome do Projeto ')
+   caminho = `./${nomeDoProjeto}`
+   if(fs.existsSync(caminho)) throw "Project Already Exist!";
+   fs.mkdirSync(caminho);
+   fs.appendFile(`${caminho}/index.html`,EMMET,function(err){if(err) throw err;console.log('Html criado')})
+   fs.appendFile(`${caminho}/style.css`,RESET,function(err){if(err)throw err;console.log('Css Criado')})
+   fs.appendFile(`${caminho}/script.js`,"// Js vazio",function(err){if(err)throw err;console.log('Js Criado')})
+}
+
+
+
+
+
+
+
+
+
+
 let EMMET = `<!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,16 +40,6 @@ let EMMET = `<!DOCTYPE html>
 </html>"
 `
 let RESET = `*{margin:0;padding:0;box-sizing:border-box;}`
-createWebProject();
 
-function createWebProject(){
-   
-   console.log("Default names:\nindex.html\nstyle.css\nscript.js\nVocê pode renomear dps");
-   nomeDoProjeto = prompt('Nome do Projeto ')
-   caminho = `./${nomeDoProjeto}`
-   if(fs.existsSync(caminho)) throw "Project Already Exist!";
-   fs.mkdirSync(caminho);
-   fs.appendFile(`${caminho}/index.html`,EMMET,function(err){if(err) throw err;console.log('Html criado')})
-   fs.appendFile(`${caminho}/style.css`,RESET,function(err){if(err)throw err;console.log('Css Criado')})
-   fs.appendFile(`${caminho}/script.js`,"// Js vazio",function(err){if(err)throw err;console.log('Js Criado')})
-}
+
+
